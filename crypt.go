b3c810bd7e4990102ed49710d198cbd14d464dd7
chenchun_lei <chenchun_lei@fzzixun.com>
2022-11-22 09:20:12 +0800

    111

---
+++ crypt.go
@@ -0,0 +1,30 @@
+package go_anti_spider
+
+// token加密
+func TEnCrypt() {
+}
+
+// token解密
+func TDeCrypt() {
+
+}
+
+// packet加密
+func PEnCrypt() {
+
+}
+
+// packet解密
+func PDeCrypt() {
+
+}
+
+// visitor加密
+func VEnCrypt() {
+
+}
+
+// visitor解密
+func VDeCrypt() {
+
+}
